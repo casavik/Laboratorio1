9f1e5bafec2eb3d8c5f52d75a523b47a8fd3bd19
Karina <casavik@gmail.com>
2015-07-11 21:05:16 -0600

    Main Test

--- Laboratorio1/Program.cs
+++ Laboratorio1/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Laboratorio1
+
+namespace Laboratorio1.Integral
 {
     class Program
     {
         static void Main(string[] args)
         {
+           Integracion integra = new Integracion();
+
+           double valorA = 5.0;
+           double VAlorB = 10.0;
+           double valorK = 40.0;
+           double valorN = 10.0;
+           int tipo = 6;
+           double res = 0.0;
+
+           double result = Math.Sin(40);
+            res = integra.ResolverIntegral(valorA, VAlorB, valorK, valorN, tipo);
+            double numero = Math.Round(res, 4);
+            double resuktado = 1.3;
+            double resuktad = 1.3;
         }
     }
 }
